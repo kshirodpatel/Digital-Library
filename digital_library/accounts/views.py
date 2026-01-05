6005949ed8d622deaf0bd33aa15f810244c1f3e0
Kshirod Patel <kshirod.patel@gmail.com>
2026-01-05 20:29:07 +0530

    Added inital views

--- digital_library/accounts/views.py
+++ digital_library/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+    context = {}
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'accounts/login', context)
