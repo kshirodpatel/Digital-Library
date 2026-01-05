48efdc5c355a55b963f4be71918f0fd6a47b89d6
Kshirod Patel <kshirod.patel@gmail.com>
2026-01-05 20:29:57 +0530

    Updated views

--- digital_library/library/views.py
+++ digital_library/library/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def browseBooks(request):
+    context = {}
+    return render(request, 'books.html', context)
+
+def bookDetails(request):
+    context = {}
+    return render(request, 'book-details.html', context)
