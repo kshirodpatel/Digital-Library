396c0e2db7cfc7f60926d2ca0e1987d979d3f47d
Kshirod Patel <kshirod.patel@gmail.com>
2026-01-16 17:01:37 +0530

    Updated URLs

--- digital_library/accounts/urls.py
+++ digital_library/accounts/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+# from accounts import views as views
+# from django.contrib.auth import views as auth_views
 from . import views
+# from .views import RegisterView, MyLoginView, UserProfileView
+
+# urlpatterns = [
+#     path("login/", MyLoginView.as_view(), name="login"),
+#     path("register/", RegisterView.as_view(), name='register' ),
+#     path("logout/", auth_views.LogoutView.as_view(), name='logout' ),
+#     path("profile/", UserProfileView.as_view(), name='user-profile'),
+# ]
 
 urlpatterns = [
-    path( 'register/', views.register, name='register' ),
-    path( 'login/', views.login, name='login' )
+    path("register/", views.RegisterUser, name="register")
 ]
