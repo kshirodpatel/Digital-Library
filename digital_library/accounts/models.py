6d3cb9c7c742718ba24b8f2910cea8a6d5c3ce5a
Kshirod Patel <kshirod.patel@gmail.com>
2026-01-16 17:01:23 +0530

    Added phone number

--- digital_library/accounts/models.py
+++ digital_library/accounts/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.username
