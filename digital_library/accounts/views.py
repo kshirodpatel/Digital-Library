7effc54a2afd3123845c521644ae36064b7004c0
Kshirod Patel <kshirod.patel@gmail.com>
2026-01-16 17:01:49 +0530

    Updated views

--- digital_library/accounts/views.py
+++ digital_library/accounts/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.urls import reverse_lazy
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import MyUserCreationForm
 
-# Create your views here.
-def register(request):
-    context = {}
-    return render(request, 'accounts/register.html', context)
+# from django.contrib.auth import login
+# from django.contrib.auth.models import User
+# from django.views.generic.edit import CreateView
+# from .forms import CustomUserCreationForm
+# from django.contrib.auth.views import LoginView
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.views.generic import DetailView
+# from django.contrib.auth.forms import UserCreationForm
 
-def login(request):
-    context = {}
-    return render(request, 'login.html', context)
+def RegisterUser(request):
+    form = MyUserCreationForm()
+    context = {"form":form}
+
+    if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+        
+
+    context = {"form":form}
+    return render(request, "accounts/register.html", context)
+
+# def RegisterView(request):
+#     form = UserCreationForm()
+#     return render(request, "accounts/register.html", {"form":form})
+#     # form_class = CustomUserCreationForm
+#     # success_url = reverse_lazy('accounts:login')
+#     # template_name = "accounts/register.html"
+
+#     # def dispatch(self, request, *args, **kwargs):
+#     #     if request.user.is_authenticated:
+#     #         return redirect("/")
+#     #     return super().dispatch(request, *args, **kwargs)
+
+#     # def form_valid(self, form):
+#     #     response = super().form_valid(form)
+#     #     login(self.request, self.object)
+#     #     return redirect("/")
+
+
+# class UserProfileView(LoginRequiredMixin, DetailView):
+#     model = User
+#     template_name = 'accounts/profile.html'
+#     context_object_name = 'profile_user'
+
+#     def get_object(self):
+#         return self.request.user
+
+# class MyLoginView(LoginView):
+#     template_name = 'accounts/login.html'
+#     redirect_authenticated_user = True  # Prevents logged-in users from seeing login page
